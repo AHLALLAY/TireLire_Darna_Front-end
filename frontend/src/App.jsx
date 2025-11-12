5062b3b8b0cf7f53acaac6be585e967a8998a68a
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-11-12 21:44:11 +0100

    feat: configuration du routage avec React Router
    - Configuration des routes principales
    - Route pour la page d'accueil visiteur (/)
    - Route pour la page de connexion (/login)
    - Route pour la page d'inscription (/signup)
    - Intégration du Layout avec Outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VisiteurHome from "./pages/home/visiteurHome";
+import Layout from "./components/layout/layout";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 function App() {
 
-
   return (
-    <>
-      <div className="placeholder">this is app.jsx</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<VisiteurHome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          
+        </Route>
+      </Routes>
+   </BrowserRouter>
   )
 }
 
